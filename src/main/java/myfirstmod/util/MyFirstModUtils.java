7b62a506a688f399d707ffd6d40cdca87fa06313
cano <mahorowa3088@gmail.com>
2019-09-08 16:50:00 +0900

    resource loader

---
+++ src/main/java/myfirstmod/util/MyFirstModUtils.java
@@ -0,0 +1,25 @@
+package myfirstmod.util;
+
+import myfirstmod.common.MyFirstMod;
+import net.minecraft.util.ResourceLocation;
+
+public class MyFirstModUtils {
+
+  public static ResourceLocation getResource(ResourceType type, String name) {
+    return new ResourceLocation(MyFirstMod.MODID, type.get() + name);
+  }
+
+  public static enum ResourceType {
+    GUI("textures/gui/"), BLOCK("textures/blocks/"), ITEMS("textures/items/"), MODEL("models/");
+
+    private String prefix;
+
+    ResourceType(String s) {
+      prefix = s;
+    }
+
+    public String get() {
+      return prefix;
+    }
+  }
+}
