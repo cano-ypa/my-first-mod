f87b5951937f90336b3c8e54e97d7b1bbdd0c2f5
cano <mahorowa3088@gmail.com>
2019-09-10 08:34:17 +0900

    ボタンのテスト

--- src/main/java/myfirstmod/client/gui/GuiTest.java
+++ src/main/java/myfirstmod/client/gui/GuiTest.java
@@ -4,6 +4,7 @@ import myfirstmod.blocks.tile.TileNull;
 import myfirstmod.container.ContainerNull;
 import myfirstmod.util.MyFirstModUtils;
 import myfirstmod.util.MyFirstModUtils.ResourceType;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.IInventory;
 import net.minecraftforge.fml.relauncher.Side;
@@ -21,6 +22,13 @@ public class GuiTest extends GuiContainer {
     this.drawDefaultBackground();
     this.mc.getTextureManager().bindTexture(MyFirstModUtils.getResource(ResourceType.GUI, "my_first_gui.png"));
     this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, 176, 166);
+  }
+
+  @Override
+  public void initGui() {
+    super.initGui();
+
+    this.buttonList.add(new GuiButton(1, 0, 0, 100, 20, "Hello World!!"));
   }
 
 }
