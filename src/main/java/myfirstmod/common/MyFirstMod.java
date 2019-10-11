e3c467d210cb9deb69c31e02c088dcb7383715a4
Cano <developer.sasazono@gmail.com>
2019-10-11 15:45:24 +0900

    🗑️ 不要な記述を削除

--- src/main/java/myfirstmod/common/MyFirstMod.java
+++ src/main/java/myfirstmod/common/MyFirstMod.java
@@ -3,13 +3,8 @@ package myfirstmod.common;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import myfirstmod.blocks.tile.TileMyFirstGui;
-import myfirstmod.client.CreativeTabMyFirstMod;
-import myfirstmod.client.ModelRegistrationHandler;
 import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod;
@@ -20,14 +15,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = ModInfo.MODID, name = ModInfo.MODNAME, version = ModInfo.MODVERSION, guiFactory = ModInfo.GUIFACTORY, acceptedMinecraftVersions = ModInfo.MCVERSION)
 @EventBusSubscriber()
 public class MyFirstMod {
-
-  public static final CreativeTabs tabMyFirstMod = new CreativeTabMyFirstMod();
 
   @Instance(ModInfo.MODID)
   public static MyFirstMod instance;
@@ -40,13 +31,6 @@ public class MyFirstMod {
 
   @EventHandler
   public void init(FMLInitializationEvent event) {
-    // GUI
-    NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
-
-    // Tile Entity の登録
-    final TileMyFirstGui TILE_MYFIRSTGUI = new TileMyFirstGui();
-    GameRegistry.registerTileEntity(TILE_MYFIRSTGUI.getClass(),
-        new ResourceLocation("myfirstmod", "my_first_gui_block"));
   }
 
   @EventHandler
@@ -55,19 +39,14 @@ public class MyFirstMod {
 
   @SubscribeEvent
   public static void registerBlocks(Register<Block> event) {
-    MyFirstModBlocks.registerBlocks(event.getRegistry());
   }
 
   @SubscribeEvent
   public static void registerItems(Register<Item> event) {
-    MyFirstModItems.registerItems(event.getRegistry());
-    MyFirstModItems.registerItemBlocks(event.getRegistry());
   }
 
   @SubscribeEvent
   public static final void registerModels(ModelRegistryEvent event) {
-    ModelRegistrationHandler.registerBlockModels();
-    ModelRegistrationHandler.registerItemModels();
   }
 
 }
