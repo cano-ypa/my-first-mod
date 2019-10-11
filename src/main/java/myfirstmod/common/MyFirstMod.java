811068bfbb089a859a85864dbae1becdaaef493b
Cano <developer.sasazono@gmail.com>
2019-10-11 15:55:02 +0900

    🗑️ 不要なコードを削除

--- src/main/java/myfirstmod/common/MyFirstMod.java
+++ src/main/java/myfirstmod/common/MyFirstMod.java
@@ -3,21 +3,14 @@ package myfirstmod.common;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ModInfo.MODID, name = ModInfo.MODNAME, version = ModInfo.MODVERSION, guiFactory = ModInfo.GUIFACTORY, acceptedMinecraftVersions = ModInfo.MCVERSION)
-@EventBusSubscriber()
 public class MyFirstMod {
 
   @Instance(ModInfo.MODID)
@@ -35,18 +28,6 @@ public class MyFirstMod {
 
   @EventHandler
   public void postInit(FMLPostInitializationEvent event) {
-  }
-
-  @SubscribeEvent
-  public static void registerBlocks(Register<Block> event) {
-  }
-
-  @SubscribeEvent
-  public static void registerItems(Register<Item> event) {
-  }
-
-  @SubscribeEvent
-  public static final void registerModels(ModelRegistryEvent event) {
   }
 
 }
