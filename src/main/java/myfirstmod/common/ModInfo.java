40502a038d2e84da07e9b7b28f986041e88e9a98
Cano <developer.sasazono@gmail.com>
2019-10-14 18:11:51 +0900

    ⚡ バージョン名を変更

--- src/main/java/myfirstmod/common/ModInfo.java
+++ src/main/java/myfirstmod/common/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
   public static final String MODID = "myfirstmod";
   public static final String MODNAME = "My First Mod";
-  public static final String MODVERSION = "0.0.1";
+  public static final String MODVERSION = "Egg";
   public static final String MCVERSION = "[1.12.2]";
 
 }
