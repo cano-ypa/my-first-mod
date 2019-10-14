99703ed299dcf07d90f13a98db8e02c9eb57cf4c
Cano <developer.sasazono@gmail.com>
2019-10-14 22:37:48 +0900

    ⚡ version name として modinfo に移動

--- src/main/java/myfirstmod/common/ModInfo.java
+++ src/main/java/myfirstmod/common/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 
   public static final String MODID = "myfirstmod";
   public static final String MODNAME = "My First Mod";
-  public static final String MODVERSION = "Egg";
+  public static final String MODVERSION = "gradle:version";
   public static final String MCVERSION = "[1.12.2]";
 
 }
