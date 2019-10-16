2db5862b1b0466b817f7ed13daae9e1a2639583c
Cano <developer.sasazono@gmail.com>
2019-10-16 10:36:01 +0900

    🚧 addGuiElement を追加

--- src/main/java/myfirstmod/client/gui/GuiBase.java
+++ src/main/java/myfirstmod/client/gui/GuiBase.java
@@ -1,8 +1,13 @@
 package myfirstmod.client.gui;
 
+import java.util.ArrayList;
+
+import myfirstmod.client.gui.element.GuiElement;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GuiBase extends GuiScreen {
+
+  public ArrayList<GuiElement> elements = ArrayList<GuiElement>();
 
   public GuiBase() {
   }
@@ -15,6 +20,10 @@ public class GuiBase extends GuiScreen {
   @Override
   public void drawScreen(int mouseX, int mouseY, float partialTicks) {
     super.drawScreen(mouseX, mouseY, partialTicks);
+  }
+
+  public addGuiElement(GuiElement element){
+    this.elements.add(element);
   }
 
 }
