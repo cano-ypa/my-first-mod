f781630a1efa49f77f7515c46b9a55b08476122f
Cano <developer.sasazono@gmail.com>
2019-10-16 10:32:22 +0900

    🚧 initGui, drawScreen を追加

--- src/main/java/myfirstmod/client/gui/GuiBase.java
+++ src/main/java/myfirstmod/client/gui/GuiBase.java
@@ -7,4 +7,14 @@ public class GuiBase extends GuiScreen {
   public GuiBase() {
   }
 
+  @Override
+  public void initGui() {
+    super.initGui();
+  }
+
+  @Override
+  public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+    super.drawScreen(mouseX, mouseY, partialTicks);
+  }
+
 }
