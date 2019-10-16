501d6d8011f7b03b2ef9efc9b465eb07b8162a47
Cano <developer.sasazono@gmail.com>
2019-10-16 10:55:21 +0900

    🚧 拡張

--- src/main/java/myfirstmod/client/gui/element/GuiButtonElement.java
+++ src/main/java/myfirstmod/client/gui/element/GuiButtonElement.java
@@ -1,8 +1,22 @@
 package myfirstmod.client.gui.element;
 
-public class GuiButtonElement {
+public class GuiButtonElement extends GuiElement {
 
-  public GuiButtonElement(String text, int x, int y, int w, int h) {
+  private String text;
+  private int posX = 0;
+  private int posY = 0;
+  private int width = 200;
+  private int height = 20;
+
+  public GuiButtonElement(String text, int posX, int posY, int width, int height) {
+    this.text = text;
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
+  }
+
+  public void draw() {
 
   }
 
