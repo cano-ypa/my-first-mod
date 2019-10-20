06ab2532e1ef5a25e1d7808dd91ce054c0b7f296
Cano <developer.sasazono@gmail.com>
2019-10-20 13:42:38 +0900

    ✨ 汎用メソッドを追加

--- src/main/java/myfirstmod/client/gui/GuiBase.java
+++ src/main/java/myfirstmod/client/gui/GuiBase.java
@@ -3,9 +3,12 @@ package myfirstmod.client.gui;
 import java.util.ArrayList;
 
 import myfirstmod.client.gui.element.GuiElement;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GuiBase extends GuiScreen {
+
+  private FontRenderer fontRenderer = mc.fontRenderer;
 
   public ArrayList<GuiElement> elements = new ArrayList<GuiElement>();
 
@@ -26,6 +29,14 @@ public class GuiBase extends GuiScreen {
 
   public void addGuiElement(GuiElement element) {
     this.elements.add(element);
+  }
+
+  public void drawTexturedRect(int x, int y, int textureX, int textureY, int width, int height) {
+    this.drawTexturedModalRect(x, y, textureX, textureY, width, height);
+  }
+
+  public void drawString(String text, int x, int y, int color) {
+    this.drawCenteredString(fontRenderer, text, x, y, color);
   }
 
 }
